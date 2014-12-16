ec8d913f32cd619d8e37c17c1c54306e2452b350
Jaitl <jaitl@outlook.com>
2014-12-17 00:28:18 +0300

    add log4j

--- src/main/java/com/jaitlpro/usercryptodb/Endpoint.java
+++ src/main/java/com/jaitlpro/usercryptodb/Endpoint.java
@@ -1,11 +1,16 @@
 package com.jaitlpro.usercryptodb;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Endpoint {
+
+    static final Logger log = Logger.getLogger(Endpoint.class);
     public static void main(String[] args) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("spring/spring-context.xml");
+
+        log.info("log work well!");
     }
 }
