6c3b5444c936b9ab4dd73324a2870ccbcaa0b04e
Jaitl <jaitl@outlook.com>
2014-12-17 00:08:08 +0300

    Add spring

--- src/main/java/com/jaitlpro/usercryptodb/Endpoint.java
+++ src/main/java/com/jaitlpro/usercryptodb/Endpoint.java
@@ -1,7 +1,11 @@
 package com.jaitlpro.usercryptodb;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Endpoint {
     public static void main(String[] args) {
-
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("spring/spring-context.xml");
     }
 }
